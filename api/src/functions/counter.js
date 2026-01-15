aaeb55e3a2981a8de074fd63a720e6f632e13a97
Jay-O-A <103457497+Jay-O-A@users.noreply.github.com>
2026-01-14 19:47:19 -0500

    Updated: Counter.js for Node v4 model

--- api/src/functions/counter.js
+++ api/src/functions/counter.js
@@ -1,44 +1,16 @@
-const { TableClient } = require("@azure/data-tables");
+const { app } = require("@azure/functions");
 
-const tableName = "visitors";
-const partitionKey = "resume";
-const rowKey = "count";
+let count = 0;
 
-module.exports = async function (context, req) {
-  const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-  const tableClient = TableClient.fromConnectionString(
-    connectionString,
-    tableName
-  );
-
-  try {
-    await tableClient.createTable();
-  } catch (e) {
-    // table already exists
+app.http("counter", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: async (request, context) => {
+    count++;
+    return {
+      status: 200,
+      jsonBody: { count }
+    };
   }
-
-  let count = 1;
-
-  try {
-    const entity = await tableClient.getEntity(partitionKey, rowKey);
-    count = entity.count + 1;
-    entity.count = count;
-    await tableClient.updateEntity(entity, "Replace");
-  } catch {
-    await tableClient.createEntity({
-      partitionKey,
-      rowKey,
-      count,
-    });
-  }
-
-  context.res = {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: { count },
-  };
-};
+});
 
